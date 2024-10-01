1a42b35f47626a6492cbf4b3863180ba453a9ec8
wangonya <kwangonya@gmail.com>
2024-10-01 15:37:22 +0300

    bool

---
+++ 03/bool.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void bool_1(void)
+{
+    int x = 1;
+    if (x)
+    {
+        printf("bool_1 x is true!\n");
+    }
+}
+
+void bool_2(void)
+{
+    bool x = true;
+    if (x)
+    {
+        printf("bool_2 x is true!\n");
+    }
+}
+
+int main(void)
+{
+    bool_1();
+    bool_2();
+}
