d0b23a577bc6ca41645d6b5a33620ea3269e3bed
wangonya <kwangonya@gmail.com>
2024-10-01 15:40:46 +0300

    update output

--- 03/ternary.c
+++ 03/ternary.c
@@ -7,5 +7,6 @@ int main(void)
 
     // if x > 10, add 17 to y. Otherwise add 37 to y.
     y += x > 10 ? 17 : 37;
-    printf("y = %d", y);
+    printf("y = %d\n", y);
+    printf("The number %d is %s\n", y, y % 2 == 0 ? "even" : "odd");
 }
