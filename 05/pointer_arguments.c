dd32a6ccc42f39cf4b5659243f16aff8d252c932
wangonya <kwangonya@gmail.com>
2024-10-16 08:11:17 +0300

    pointers

---
+++ 05/pointer_arguments.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void increment(int *p)
+{
+    *p = *p + 1;
+}
+int main(void)
+{
+    int i = 10;
+    int *j = &i;
+
+    printf("i is %d\n", i);
+    printf("i is also %d\n", *j);
+
+    increment(j);
+
+    printf("i is %d\n", i);
+}
