a84932ddee5265c47aa421247d1e0d02270a3b5a
wangonya <kwangonya@gmail.com>
2024-11-06 17:26:26 +0300

    17

---
+++ 17/bar.h
@@ -0,0 +1,6 @@
+#ifndef BAR_H // if BAR_H isn't defined
+#define BAR_H // defined it (with no particular value)
+
+int add(int, int);
+
+#endif
---
+++ 17/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "bar.h"
+
+int main(void)
+{
+    printf("%d\n", add(2, 3)); // 5!
+}
